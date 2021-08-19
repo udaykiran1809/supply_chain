a30d9c9cc679aa0bb216dcdd5187012cedd7a200
Uday Kiran <70482052+udaykiran1809@users.noreply.github.com>
2021-08-19 22:31:25 +0530

    Update tests.rs

--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -1,23 +1,20 @@
-use crate::{Error, mock::*};
-use frame_support::{assert_ok, assert_noop};
+use crate::{mock::*, Error};
+use frame_support::{assert_noop, assert_ok};
 
 #[test]
-fn it_stores_correctly() {
+fn it_works_for_default_value() {
 	new_test_ext().execute_with(|| {
 		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::store_land(Origin::signed(1),"Vamshi", [1,2,3,4]));
+		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
 		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::proof(), Some("Vamshi", [1,2,3,4]));
+		assert_eq!(TemplateModule::something(), Some(42));
 	});
 }
 
-// #[test]
-// fn correct_error_for_none_value() {
-// 	new_test_ext().execute_with(|| {
-// 		// Ensure the expected error is thrown when no value is present.
-// 		assert_noop!(
-// 			TemplateModule::cause_error(Origin::signed(1)),
-// 			Error::<Test>::NoneValue
-// 		);
-// 	});
-// }
+#[test]
+fn correct_error_for_none_value() {
+	new_test_ext().execute_with(|| {
+		// Ensure the expected error is thrown when no value is present.
+		assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
+	});
+}
